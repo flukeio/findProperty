d09b52364723e0ee1c136ce384cf4bd2fc2db8b8
Hussein Omar <account@flukeio.com>
2021-08-21 01:44:57 +0300

    init commit

---
+++ findProperty/findProperty/findPropertyApp.swift
@@ -0,0 +1,17 @@
+//
+//  findPropertyApp.swift
+//  findProperty
+//
+//  Created by User on 21/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct findPropertyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
